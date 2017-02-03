7882691fbe7964672e683eba456ecca3c1bf64a9
rodneystrong <akang2@gmail.com>
2017-02-03 15:26:50 -0800

    removed initial test code and replaced with our first component

--- app/index.js
+++ app/index.js
@@ -1,2 +1,17 @@
-var app = document.querySelector('#app');
-app.innerHTML = 'hello!';
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+//createClass is a method that will create a new React component
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div>Heeeeey World</div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.querySelector('#app')
+);
